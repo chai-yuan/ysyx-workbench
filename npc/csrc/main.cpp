959f82acadf64857a56575c14d76991c417b71de
chai-yuan <charain_li@outlook.com>
2023-07-16 07:49:30 +0800

    feat(npc):搭建verilator开发环境并运行示例程序

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "Vexample.h"
+#include "verilated.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vexample* top = new Vexample{contextp};
+    while (!contextp->gotFinish()) {
+        top->eval();
+    }
+    delete top;
+    delete contextp;
+    return 0;
 }
