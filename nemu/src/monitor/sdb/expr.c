a3b5f144d972f08ca4d633b5ad6b5c721caafb0a
chai-yuan <charain_li@outlook.com>
2023-07-21 00:15:56 +0800

    feat(nemu)：完成表达式解析，表达式生成小工具

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 
@@ -21,24 +21,24 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
-
-  /* TODO: Add more token types */
-
+    TK_NOTYPE = 256,
+    TK_EQ,
+    TK_DECIMAL
 };
 
 static struct rule {
-  const char *regex;
-  int token_type;
+    const char* regex;
+    int token_type;
 } rules[] = {
-
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
-
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
+    {" +", TK_NOTYPE},       // spaces
+    {"\\+", '+'},            // plus
+    {"==", TK_EQ},           // equal
+    {"\\-", '-'},            // minus
+    {"\\*", '*'},            // multiply
+    {"\\/", '/'},            // divide
+    {"\\(", '('},            // left parenthesis
+    {"\\)", ')'},            // right parenthesis
+    {"[0-9]+", TK_DECIMAL},  // decimal number
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -49,77 +49,202 @@ static regex_t re[NR_REGEX] = {};
  * Therefore we compile them only once before any usage.
  */
 void init_regex() {
-  int i;
-  char error_msg[128];
-  int ret;
+    int i;
+    char error_msg[128];
+    int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
-    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
-      regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    for (i = 0; i < NR_REGEX; i++) {
+        ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+        if (ret != 0) {
+            regerror(ret, &re[i], error_msg, 128);
+            panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+        }
     }
-  }
 }
+
+#define TOKEN_BUF_SIZE 128
 
 typedef struct token {
-  int type;
-  char str[32];
+    int type;
+    char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;
+static Token tokens[TOKEN_BUF_SIZE] __attribute__((used)) = {};
+static int nr_token __attribute__((used)) = 0;
 
-static bool make_token(char *e) {
-  int position = 0;
-  int i;
-  regmatch_t pmatch;
+static bool make_token(char* e) {
+    int position = 0;
+    int i;
+    regmatch_t pmatch;
 
-  nr_token = 0;
+    nr_token = 0;
 
-  while (e[position] != '\0') {
-    /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        char *substr_start = e + position;
-        int substr_len = pmatch.rm_eo;
+    while (e[position] != '\0') {
+        /* Try all rules one by one. */
+        for (i = 0; i < NR_REGEX; i++) {
+            if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+                char* substr_start = e + position;
+                int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+                Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+                    i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
-        position += substr_len;
+                position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
+                /* TODO: Now a new token is recognized with rules[i]. Add codes
+                 * to record the token in the array `tokens'. For certain types
+                 * of tokens, some extra actions should be performed.
+                 */
+                Assert(nr_token < TOKEN_BUF_SIZE, "Too many tokens");
+                switch (rules[i].token_type) {
+                    case TK_NOTYPE:
+                        break;
+                    case TK_EQ:
+                        tokens[nr_token].type = TK_EQ;
+                        nr_token++;
+                        break;
+                    case TK_DECIMAL:
+                        tokens[nr_token].type = TK_DECIMAL;
+                        Assert(substr_len < 32, "Number length too long");
+                        strncpy(tokens[nr_token].str, substr_start, substr_len);
+                        nr_token++;
+                        break;
+                    case '+':
+                        tokens[nr_token].type = '+';
+                        nr_token++;
+                        break;
+                    case '-':
+                        tokens[nr_token].type = '-';
+                        nr_token++;
+                        break;
+                    case '*':
+                        tokens[nr_token].type = '*';
+                        nr_token++;
+                        break;
+                    case '/':
+                        tokens[nr_token].type = '/';
+                        nr_token++;
+                        break;
+                    case '(':
+                        tokens[nr_token].type = '(';
+                        nr_token++;
+                        break;
+                    case ')':
+                        tokens[nr_token].type = ')';
+                        nr_token++;
+                        break;
+                    default:
+                        panic("Unknown token type");
+                }
 
-        switch (rules[i].token_type) {
-          default: TODO();
+                break;
+            }
         }
 
-        break;
-      }
+        if (i == NR_REGEX) {
+            printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+            return false;
+        }
     }
 
-    if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
-    }
-  }
-
-  return true;
+    return true;
 }
 
+bool check_parentheses(int p, int q) {
+    int left_cnt = 0;
 
-word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
-    *success = false;
+    for (int i = p; i <= q; i++) {
+        if (tokens[p].type == '(')
+            left_cnt++;
+        else if (tokens[p].type == ')')
+            left_cnt--;
+
+        if (left_cnt < 1 && i != q)
+            return false;
+    }
+    return left_cnt != 0;
+}
+
+int get_priority(char token_type) {
+    switch (token_type) {
+        case '*':
+        case '/':
+            return 1;
+        case '+':
+        case '-':
+            return 2;
+        default:
+            return 0;
+    }
+}
+
+int find_main_operator(int p, int q) {
+    int ret = p, left_cnt = 0;
+    char now_op = '?';
+    for (int i = p; i <= q; i++) {
+        if (tokens[i].type == '(') {
+            left_cnt++;
+            i++;
+            while (true) {
+                if (tokens[i].type == '(')
+                    left_cnt++;
+                else if (tokens[i].type == ')')
+                    left_cnt--;
+                i++;
+                if (left_cnt == 0)
+                    break;
+            }
+            if (i > q)
+                break;
+        } else if (tokens[i].type == TK_DECIMAL)
+            continue;
+        else if (get_priority(tokens[i].type) >= get_priority(now_op)) {
+            now_op = tokens[i].type;
+            ret = i;
+        }
+    }
+    return ret;
+}
+
+uint32_t eval(int p, int q) {
+    if (p > q) {
+        printf("%d,%d", p, q);
+        panic("Bad expression");
+    } else if (p == q) {
+        uint32_t ret = 0;
+        sscanf(tokens[p].str, "%d", &ret);
+        return ret;
+    } else if (check_parentheses(p, q) == true) {
+        // The expression is surrounded by a matched pair of parentheses.
+        // If that is the case, just throw away the parentheses.
+        return eval(p + 1, q - 1);
+    } else {
+        int op = find_main_operator(p, q);  // find the position of 主运算符 in the token expression
+        uint32_t val1 = eval(p, op - 1);
+        uint32_t val2 = eval(op + 1, q);
+
+        switch (tokens[op].type) {
+            case '+':
+                return val1 + val2;
+            case '-':
+                return val1 - val2;
+            case '*':
+                return val1 * val2;
+            case '/':
+                return val1 / val2;
+            default:
+                panic("Operator does not exist");
+        }
+    }
     return 0;
-  }
+}
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+word_t expr(char* e, bool* success) {
+    if (!make_token(e)) {
+        *success = false;
+        return 0;
+    }
 
-  return 0;
+    *success = true;
+    return eval(0, nr_token - 1);
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,69 +1,130 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <assert.h>
 #include <string.h>
+#include <time.h>
 
 // this should be enough
-static char buf[65536] = {};
-static char code_buf[65536 + 128] = {}; // a little larger than `buf`
-static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
+#define BUF_SIZE 4096
+static char buf[BUF_SIZE] = {};
+static int buf_idx;
+static char code_buf[BUF_SIZE + 128] = {};  // a little larger than `buf`
+static char* code_format =
+    "#include <stdio.h>\n"
+    "int main() { "
+    "  unsigned result = %s; "
+    "  printf(\"%%u\", result); "
+    "  return 0; "
+    "}";
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
+int choose(int n) {
+    if (buf_idx > 64)
+        return 0;
+    return rand() % n;
 }
 
-int main(int argc, char *argv[]) {
-  int seed = time(0);
-  srand(seed);
-  int loop = 1;
-  if (argc > 1) {
-    sscanf(argv[1], "%d", &loop);
-  }
-  int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+void gen(char c) {
+    if (buf_idx >= BUF_SIZE - 1) {
+        printf("Buffer overflow!\n");
+        exit(1);
+    }
+    buf[buf_idx++] = c;
+}
 
-    sprintf(code_buf, code_format, buf);
+void gen_num() {
+    int num = rand() % 100;  // Generate a random number less than 1000
+    int len = snprintf(buf + buf_idx, BUF_SIZE - buf_idx, "%d", num);
+    if (len >= BUF_SIZE - buf_idx) {
+        printf("Buffer overflow!\n");
+        exit(1);
+    }
+    buf_idx += len;
+    // gen('U');
+}
 
-    FILE *fp = fopen("/tmp/.code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
+void gen_rand_op() {
+    char ops[] = "+-*/";
+    gen(ops[choose(4)]);  // Choose a random operator from ops
+}
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+void gen_rand_space() {
+    for (int i = 0; i < choose(4); i++) {
+        gen(' ');
+    }
+}
 
-    fp = popen("/tmp/.expr", "r");
-    assert(fp != NULL);
+void gen_rand_expr() {
+    // printf("%s\n", buf);
+    switch (choose(3)) {
+        case 0:
+            gen_num();
+            gen_rand_space();
+            break;
+        case 1:
+            gen('(');
+            gen_rand_space();
+            gen_rand_expr();
+            gen(')');
+            break;
+        default:
+            gen_rand_expr();
+            gen_rand_space();
+            gen_rand_op();
+            gen_rand_space();
+            gen_rand_expr();
+            break;
+    }
+}
 
-    int result;
-    ret = fscanf(fp, "%d", &result);
-    pclose(fp);
+int main(int argc, char* argv[]) {
+    int seed = time(0);
+    srand(seed);
+    int loop = 1;
+    if (argc > 1) {
+        sscanf(argv[1], "%d", &loop);
+    }
+    int i;
+    for (i = 0; i < loop; i++) {
+        buf_idx = 0;
+        memset(buf, 0, sizeof(buf));
+        gen_rand_expr();
 
-    printf("%u %s\n", result, buf);
-  }
-  return 0;
+        sprintf(code_buf, code_format, buf);
+
+        FILE* fp = fopen("/tmp/.code.c", "w");
+        assert(fp != NULL);
+        fputs(code_buf, fp);
+        fclose(fp);
+
+        int ret = system("gcc -Werror /tmp/.code.c -o /tmp/.expr");
+        if (ret != 0) {
+            continue;
+        }
+
+        fp = popen("/tmp/.expr", "r");
+        assert(fp != NULL);
+
+        int result;
+        ret = fscanf(fp, "%d", &result);
+        pclose(fp);
+
+        printf("%u %s\n", result, buf);
+    }
+    return 0;
 }
