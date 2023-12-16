2c13de455d1d0d265b5407dc9898367355ca8492
charain <charain_li@outlook.com>
2023-12-17 00:27:19 +0800

    feat(npc):添加xbar

--- npc/CRRV/src/CPUTop.scala
+++ npc/CRRV/src/CPUTop.scala
@@ -8,6 +8,7 @@ import memory.SimpleRAM
 import memory.AXIliteRAM
 import bus.Simple2AXIlite
 import bus.SimpleArbiter
+import bus.AXIliteXbar
 
 class CPUTop extends Module {
   val io = IO(new Bundle {
@@ -17,13 +18,17 @@ class CPUTop extends Module {
   val core       = Module(new Core)
   val arbiter    = Module(new SimpleArbiter)
   val simple2axi = Module(new Simple2AXIlite)
-  val ram        = Module(new AXIliteRAM(false))
+  val axixbar    = Module(new AXIliteXbar)
 
+  val ram  = Module(new AXIliteRAM(false))
+  val ram2 = Module(new AXIliteRAM(false))
   core.io.inst <> arbiter.io.simpleInst
   core.io.data <> arbiter.io.simpleData
 
   arbiter.io.simpleOut <> simple2axi.io.simple
-  simple2axi.io.axilite <> ram.io
+  simple2axi.io.axilite <> axixbar.io.in
+  axixbar.io.ram <> ram.io
+  axixbar.io.serial <> ram2.io
   // debug
   core.io.debug <> io.debug
 }
--- npc/CRRV/src/bus/AXIliteXbar.scala
+++ npc/CRRV/src/bus/AXIliteXbar.scala
@@ -5,14 +5,58 @@ import chisel3.util._
 import config.CPUconfig._
 import io._
 
+object DeviceDefine {
+  val DEVICE_WIDTH = log2Ceil(3)
+
+  val DEVICE_NONE   = 0.U(DEVICE_WIDTH.W)
+  val DEVICE_RAM    = 1.U(DEVICE_WIDTH.W)
+  val DEVICE_SERIAL = 2.U(DEVICE_WIDTH.W)
+}
+
 class AXIliteXbar extends Module {
   val io = IO(new Bundle {
-    val in   = new AXIliteMasterIO(ADDR_WIDTH, DATA_WIDTH)
-    val out0 = Flipped(new AXIliteMasterIO(ADDR_WIDTH, DATA_WIDTH))
-    val out1 = Flipped(new AXIliteMasterIO(ADDR_WIDTH, DATA_WIDTH))
+    val in     = Flipped(new AXIliteMasterIO(ADDR_WIDTH, DATA_WIDTH))
+    val ram    = new AXIliteMasterIO(ADDR_WIDTH, DATA_WIDTH)
+    val serial = new AXIliteMasterIO(ADDR_WIDTH, DATA_WIDTH)
   })
 
-  
+  val (sIdle :: sRead :: sWrite :: Nil) = Enum(3)
+  val state                             = RegInit(sIdle)
+  val device                            = RegInit(DeviceDefine.DEVICE_NONE)
 
+  switch(state) {
+    is(sIdle) {
+      when(io.in.ar.valid) { state := sRead }
+        .elsewhen(io.in.aw.valid) { state := sWrite }
+      device := MuxCase(
+        DeviceDefine.DEVICE_NONE,
+        Seq(
+          (io.in.ar.addr(31,28) === "h8".U) -> (DeviceDefine.DEVICE_RAM),
+          (io.in.ar.addr(31,28) === "ha".U) -> (DeviceDefine.DEVICE_SERIAL)
+        )
+      )
+    }
+    is(sRead) {
+      when(io.in.r.valid && io.in.r.ready) {
+        state := sIdle
+      }
+    }
+    is(sWrite) {
+      when(io.in.b.valid && io.in.b.ready) {
+        state := sIdle
+      }
+    }
+  }
 
+  when(state =/= sIdle && device === DeviceDefine.DEVICE_RAM) {
+    io.in <> io.ram
+    io.serial <> DontCare
+  }.elsewhen(state =/= sIdle && device === DeviceDefine.DEVICE_SERIAL) {
+    io.in <> io.serial
+    io.ram <> DontCare
+  }.otherwise{
+    io.in <> DontCare
+    io.ram <> DontCare
+    io.serial <> DontCare
+  }
 }
