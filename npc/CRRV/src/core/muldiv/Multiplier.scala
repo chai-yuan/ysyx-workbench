8ba8967306a0870aadac4e55e2d0dad8bb2bb85d
charain <charain_li@outlook.com>
2023-12-21 11:16:41 +0800

    feat(npc):将乘法器升级为Booth 2-bit乘法器

--- npc/CRRV/src/core/muldiv/Multiplier.scala
+++ npc/CRRV/src/core/muldiv/Multiplier.scala
@@ -8,7 +8,7 @@ import core.define.OperationDefine._
 
 /**
   * 乘法器
-  * 目前全部使用*运算符进行实现，之后再升级
+  * 使用Booth 2-bit乘法器，需要16周期完成32位乘法
   */
 class Multiplier extends Module {
   val io = IO(new Bundle {
@@ -22,12 +22,56 @@ class Multiplier extends Module {
     val opr2     = Input(UInt(DATA_WIDTH.W))
     val result   = Output(UInt((DATA_WIDTH * 2).W))
   })
-  // 改造输入操作数为有符号数
-  val opr1 = Mux(io.opr1Sign, Cat(io.opr1(DATA_WIDTH - 1), io.opr1), Cat(0.U(1.W), io.opr1))
-  val opr2 = Mux(io.opr2Sign, Cat(io.opr2(DATA_WIDTH - 1), io.opr2), Cat(0.U(1.W), io.opr2))
-  // 计算结果
-  val result = opr1.asSInt * opr2.asSInt
+  // 获得将要添加的符号位
+  val opr1Sign = Mux(io.opr1Sign, io.opr1(DATA_WIDTH - 1), 0.U(1.W))
+  val opr2Sign = Mux(io.opr1Sign, io.opr1(DATA_WIDTH - 1), 0.U(1.W))
+  // 进行有符号乘法运算
+  val opr1                               = Reg(UInt((DATA_WIDTH * 2).W))
+  val opr2                               = Reg(UInt((DATA_WIDTH + 3).W))
+  val mulResutl                          = Reg(UInt((DATA_WIDTH * 2).W))
+  val count                              = RegInit(0.U(6.W))
+  val (sIdle :: sWorking :: sEnd :: Nil) = Enum(3)
+  val state                              = RegInit(sIdle)
 
-  io.valid  := io.en && !io.flush
+  switch(state) {
+    is(sIdle) {
+      when(io.en && !io.flush) {
+        opr1      := Cat(Fill(DATA_WIDTH, opr1Sign), io.opr1) // 扩充到2倍位宽
+        opr2      := Cat(Fill(2, opr2Sign), io.opr2, 0.U(1.W)) // 扩充3位
+        mulResutl := 0.U
+        count     := 0.U
+        state     := sWorking
+      }
+    }
+    is(sWorking) {
+      when(count =/= (DATA_WIDTH / 2).U && !io.flush) {
+        mulResutl := mulResutl + MuxLookup(opr2(2, 0), 0.U)(
+          Seq(
+            ("b000".U) -> (0.U),
+            ("b001".U) -> (opr1),
+            ("b010".U) -> (opr1),
+            ("b011".U) -> (opr1 << 1),
+            ("b100".U) -> -(opr1 << 1),
+            ("b101".U) -> -(opr1),
+            ("b110".U) -> -(opr1),
+            ("b111".U) -> (0.U)
+          )
+        )
+        opr1  := opr1 << 2
+        opr2  := opr2 >> 2
+        count := count + 1.U
+      }.otherwise {
+        state := sEnd
+      }
+    }
+    is(sEnd) {
+      state := sIdle
+    }
+  }
+
+  // 计算结果
+  val result = mulResutl
+
+  io.valid  := io.en && !io.flush && state === sEnd
   io.result := result(DATA_WIDTH * 2 - 1, 0)
 }
