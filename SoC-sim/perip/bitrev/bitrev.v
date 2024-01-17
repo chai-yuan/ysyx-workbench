64e4ff641f63eb0fe46c60da95bdab3cf860a43a
charain <charain_li@outlook.com>
2024-01-17 17:27:49 +0800

    feat(soc):实现基于SPI协议的位翻转模块并测试

--- SoC-sim/perip/bitrev/bitrev.v
+++ SoC-sim/perip/bitrev/bitrev.v
@@ -2,7 +2,7 @@ module bitrev (
   input  sck,     // 时钟信号
   input  ss,      // 片选信号，低电平有效
   input  mosi,    // 主设备输出从设备输入信号
-  output reg miso // 主设备输入从设备输出信号
+  output miso // 主设备输入从设备输出信号
 );
 
 // 定义状态机的状态
@@ -10,40 +10,29 @@ typedef enum {IDLE, RECEIVE, SEND} state_t;
 state_t state = IDLE;
 
 // 用于存储接收到的数据和发送数据的寄存器
-reg [7:0] received_data = 0;
-reg [7:0] bitrev_data = 0;
-reg [7:0] bit_count = 0; // 用于计数
+reg [7:0] received_data;
+reg [7:0] bit_count; // 用于计数
 
-// 总是块，用于描述状态机的行为
-always @(posedge sck or posedge ss) begin
-  if (ss) begin
-    // 如果片选信号为高电平，重置状态机
-    state <= IDLE;
-    bit_count <= 0;
-  end else begin
+assign miso = (state == SEND) & received_data[bit_count];
+
+// 用于描述状态机的行为
+always @(posedge sck or negedge ss) begin
     case (state)
       IDLE: begin
-          // 等待片选信号变为低电平
-          if (!ss) begin
-            state <= RECEIVE;
-          end
+        if(!ss) begin
+          state <= RECEIVE;
+          bit_count <= 7;
+        end
       end
+
       RECEIVE: begin
           // 接收数据
           received_data[bit_count] <= mosi;
-          bit_count <= bit_count + 1;
-          if (bit_count == 7) begin
-            // 如果已经接收了8位数据，转到发送状态
-            state <= SEND;
-            // 计算位翻转数据
-            bitrev_data <= {received_data[0], received_data[1], received_data[2], received_data[3],
-                            received_data[4], received_data[5], received_data[6], received_data[7]};
-            bit_count <= 0; // 重置位计数器
-          end
+          bit_count <= bit_count == 0 ? 0 : bit_count - 1;
+          state <= bit_count == 0 ? SEND : RECEIVE;
       end
+
       SEND: begin
-          // 发送位翻转后的数据
-          miso <= bitrev_data[bit_count]; // 从低位向高位发送
           bit_count <= bit_count + 1;
           if (bit_count == 7) begin
             // 如果已经发送了8位数据，转回空闲状态
@@ -52,7 +41,6 @@ always @(posedge sck or posedge ss) begin
       end
       default: state <= IDLE;
     endcase
-  end
 end
 
 endmodule
