3db644f15c25788f5f19ed50132ad967ce42a6b2
charain <charain_li@outlook.com>
2025-01-12 18:01:39 +0800

    更新测试

--- flake.nix
+++ flake.nix
@@ -31,7 +31,8 @@
 
         shellHook = ''
           echo "A Nix-flake-based development environment"
-          export AM_HOME=/home/charain/Project/riscv-builder/abstract-machine
+          export AM_HOME=/home/charain/Project/ysyx-workbench/abstract-machine
+          export CREMU_HOME=/home/charain/Project/RISCV-Emulator
         '';
       };
     };
