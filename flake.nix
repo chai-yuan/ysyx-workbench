2ae5ec5053c996e1c42713815fc190abce5b73dd
charain <charain_li@outlook.com>
2025-02-10 15:47:39 +0800

    更新abstract-machine,am-kernels

--- flake.nix
+++ flake.nix
@@ -22,16 +22,20 @@
 
         packages = [
           crpkgs.riscv64-elf-gcc
+          pkgs.verilator
           pkgs.qemu
           pkgs.gcc
           pkgs.gnumake
           pkgs.bear
+          pkgs.spike
+          pkgs.dtc
           pkgs.clang-tools
         ];
 
         shellHook = ''
           echo "A Nix-flake-based development environment"
           export AM_HOME=/home/charain/Project/ysyx-workbench/abstract-machine
+          export NPC_HOME=/home/charain/Project/cpu/simulator
           export CREMU_HOME=/home/charain/Project/RISCV-Emulator
         '';
       };
